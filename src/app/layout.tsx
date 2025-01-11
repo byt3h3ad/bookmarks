1a82e7072ae2aadab32f0df25d31bb43b48f9975
byt3h3ad <adhirajd007@gmail.com>
2025-01-12 03:03:14 +0530

    feat(layout): update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,11 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "bookmarks - adhiraj",
   description: "what i read.",
+  authors: [{ name: "Adhiraj Dutta", url: "https://adhiraj.tech" }],
+  robots: {
+    index: true,
+    follow: true,
+  },
   openGraph: {
     title: "bookmarks - adhiraj",
     description: "what i read.",
