445103233f4268dbe58ff83cba61498a74084612
byt3h3ad <adhirajd007@gmail.com>
2025-01-11 20:50:52 +0530

    feat: bookmarks fetch logic

---
+++ src/utils/raindrop.ts
@@ -0,0 +1,32 @@
+import { cache } from "react";
+import "server-only";
+
+const options = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.RAINDROP_ACCESS_TOKEN}`,
+  },
+  next: {
+    revalidate: 60 * 60,
+  },
+};
+
+const RAINDROP_API_URL = "https://api.raindrop.io/rest/v1";
+
+export const getBookmarkItems = cache(async (id = 0, pageIndex = 0) => {
+  try {
+    const response = await fetch(
+      `${RAINDROP_API_URL}/raindrops/${id}?` +
+        new URLSearchParams({
+          page: pageIndex.toString(),
+          perpage: "50",
+        }),
+      options
+    );
+    return await response.json();
+  } catch (error) {
+    console.info(error);
+    return null;
+  }
+});
